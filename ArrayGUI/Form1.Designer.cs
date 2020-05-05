802d3398765085348319db5d02ed5d1a33b44821
Angie Shelton <Angie_Shelton@insideranken.org>
2020-05-04 19:55:48 -0500

    first commit

---
+++ ArrayGUI/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace ArrayGUI
+{
+    partial class FormCh8Arrays
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonNewNumbers = new System.Windows.Forms.Button();
+            this.listBoxArray = new System.Windows.Forms.ListBox();
+            this.buttonClearLlistBox = new System.Windows.Forms.Button();
+            this.buttonAvg = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonSortDescending = new System.Windows.Forms.Button();
+            this.buttonSortAscending = new System.Windows.Forms.Button();
+            this.buttonSum = new System.Windows.Forms.Button();
+            this.buttonLowest = new System.Windows.Forms.Button();
+            this.buttonHighest = new System.Windows.Forms.Button();
+            this.buttonMedian = new System.Windows.Forms.Button();
+            this.buttonRange = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonNewNumbers
+            // 
+            this.buttonNewNumbers.Location = new System.Drawing.Point(228, 36);
+            this.buttonNewNumbers.Name = "buttonNewNumbers";
+            this.buttonNewNumbers.Size = new System.Drawing.Size(210, 44);
+            this.buttonNewNumbers.TabIndex = 0;
+            this.buttonNewNumbers.Text = "New Numbers";
+            this.buttonNewNumbers.UseVisualStyleBackColor = true;
+            this.buttonNewNumbers.Click += new System.EventHandler(this.buttonNewNumbers_Click);
+            // 
+            // listBoxArray
+            // 
+            this.listBoxArray.FormattingEnabled = true;
+            this.listBoxArray.ItemHeight = 19;
+            this.listBoxArray.Location = new System.Drawing.Point(48, 36);
+            this.listBoxArray.Name = "listBoxArray";
+            this.listBoxArray.Size = new System.Drawing.Size(120, 593);
+            this.listBoxArray.TabIndex = 12;
+            // 
+            // buttonClearLlistBox
+            // 
+            this.buttonClearLlistBox.Location = new System.Drawing.Point(228, 144);
+            this.buttonClearLlistBox.Name = "buttonClearLlistBox";
+            this.buttonClearLlistBox.Size = new System.Drawing.Size(210, 44);
+            this.buttonClearLlistBox.TabIndex = 1;
+            this.buttonClearLlistBox.Text = "Clear ListBox";
+            this.buttonClearLlistBox.UseVisualStyleBackColor = true;
+            this.buttonClearLlistBox.Click += new System.EventHandler(this.buttonClearLlistBox_Click);
+            // 
+            // buttonAvg
+            // 
+            this.buttonAvg.Location = new System.Drawing.Point(526, 144);
+            this.buttonAvg.Name = "buttonAvg";
+            this.buttonAvg.Size = new System.Drawing.Size(210, 44);
+            this.buttonAvg.TabIndex = 6;
+            this.buttonAvg.Text = "Average";
+            this.buttonAvg.UseVisualStyleBackColor = true;
+            this.buttonAvg.Click += new System.EventHandler(this.buttonAvg_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(228, 585);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(210, 44);
+            this.buttonExit.TabIndex = 11;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(228, 471);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(210, 44);
+            this.buttonSearch.TabIndex = 4;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // buttonSortDescending
+            // 
+            this.buttonSortDescending.Location = new System.Drawing.Point(228, 364);
+            this.buttonSortDescending.Name = "buttonSortDescending";
+            this.buttonSortDescending.Size = new System.Drawing.Size(210, 44);
+            this.buttonSortDescending.TabIndex = 3;
+            this.buttonSortDescending.Text = "Sort Descending";
+            this.buttonSortDescending.UseVisualStyleBackColor = true;
+            this.buttonSortDescending.Click += new System.EventHandler(this.buttonSortDescending_Click);
+            // 
+            // buttonSortAscending
+            // 
+            this.buttonSortAscending.Location = new System.Drawing.Point(228, 256);
+            this.buttonSortAscending.Name = "buttonSortAscending";
+            this.buttonSortAscending.Size = new System.Drawing.Size(210, 44);
+            this.buttonSortAscending.TabIndex = 2;
+            this.buttonSortAscending.Text = "Sort Ascending";
+            this.buttonSortAscending.UseVisualStyleBackColor = true;
+            this.buttonSortAscending.Click += new System.EventHandler(this.buttonSortAscending_Click);
+            // 
+            // buttonSum
+            // 
+            this.buttonSum.Location = new System.Drawing.Point(526, 36);
+            this.buttonSum.Name = "buttonSum";
+            this.buttonSum.Size = new System.Drawing.Size(210, 44);
+            this.buttonSum.TabIndex = 5;
+            this.buttonSum.Text = "Sum";
+            this.buttonSum.UseVisualStyleBackColor = true;
+            this.buttonSum.Click += new System.EventHandler(this.buttonSum_Click);
+            // 
+            // buttonLowest
+            // 
+            this.buttonLowest.Location = new System.Drawing.Point(526, 256);
+            this.buttonLowest.Name = "buttonLowest";
+            this.buttonLowest.Size = new System.Drawing.Size(210, 44);
+            this.buttonLowest.TabIndex = 7;
+            this.buttonLowest.Text = "Lowest";
+            this.buttonLowest.UseVisualStyleBackColor = true;
+            this.buttonLowest.Click += new System.EventHandler(this.buttonLowest_Click);
+            // 
+            // buttonHighest
+            // 
+            this.buttonHighest.Location = new System.Drawing.Point(526, 364);
+            this.buttonHighest.Name = "buttonHighest";
+            this.buttonHighest.Size = new System.Drawing.Size(210, 44);
+            this.buttonHighest.TabIndex = 8;
+            this.buttonHighest.Text = "Highest";
+            this.buttonHighest.UseVisualStyleBackColor = true;
+            this.buttonHighest.Click += new System.EventHandler(this.buttonHighest_Click);
+            // 
+            // buttonMedian
+            // 
+            this.buttonMedian.Location = new System.Drawing.Point(526, 471);
+            this.buttonMedian.Name = "buttonMedian";
+            this.buttonMedian.Size = new System.Drawing.Size(210, 44);
+            this.buttonMedian.TabIndex = 9;
+            this.buttonMedian.Text = "Median";
+            this.buttonMedian.UseVisualStyleBackColor = true;
+            this.buttonMedian.Click += new System.EventHandler(this.buttonMedian_Click);
+            // 
+            // buttonRange
+            // 
+            this.buttonRange.Location = new System.Drawing.Point(526, 585);
+            this.buttonRange.Name = "buttonRange";
+            this.buttonRange.Size = new System.Drawing.Size(210, 44);
+            this.buttonRange.TabIndex = 10;
+            this.buttonRange.Text = "Range";
+            this.buttonRange.UseVisualStyleBackColor = true;
+            this.buttonRange.Click += new System.EventHandler(this.buttonRange_Click_1);
+            // 
+            // FormCh8Arrays
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Fuchsia;
+            this.ClientSize = new System.Drawing.Size(795, 643);
+            this.Controls.Add(this.buttonRange);
+            this.Controls.Add(this.buttonMedian);
+            this.Controls.Add(this.buttonHighest);
+            this.Controls.Add(this.buttonLowest);
+            this.Controls.Add(this.buttonSum);
+            this.Controls.Add(this.buttonSortAscending);
+            this.Controls.Add(this.buttonSortDescending);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonAvg);
+            this.Controls.Add(this.buttonClearLlistBox);
+            this.Controls.Add(this.listBoxArray);
+            this.Controls.Add(this.buttonNewNumbers);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "FormCh8Arrays";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ch8Arrays";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonNewNumbers;
+        private System.Windows.Forms.ListBox listBoxArray;
+        private System.Windows.Forms.Button buttonClearLlistBox;
+        private System.Windows.Forms.Button buttonAvg;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonSortDescending;
+        private System.Windows.Forms.Button buttonSortAscending;
+        private System.Windows.Forms.Button buttonSum;
+        private System.Windows.Forms.Button buttonLowest;
+        private System.Windows.Forms.Button buttonHighest;
+        private System.Windows.Forms.Button buttonMedian;
+        private System.Windows.Forms.Button buttonRange;
+    }
+}
+
---
+++ ArrayGUI/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;        //add this line to grab code from Visual Basic!
+
+namespace ArrayGUI
+{
+    public partial class FormCh8Arrays : Form
+    {
+        public FormCh8Arrays()
+        {
+            InitializeComponent();
+        }
+
+        //constant
+        const int LEN = 25;     //array length
+
+        //declare and initialize program variables
+        int[] original = new int[LEN];      //original array
+        int[] modified = new int[LEN];  //modified array
+        int theSum = 0;     //the sum of all array elements
+        double theAvg = 0.0;   //the average array element size
+
+        //This is the code that runs when the New Numbers button is clicked.
+        private void buttonNewNumbers_Click(object sender, EventArgs e)
+        {
+            fillArray();
+        }
+
+        //This method generates 25 new random numbers,
+        //each between 1 - 100 and puts them into the 
+        //original array.  As it fills up the original
+        //array, it copies the value of each array
+        //element into the modified array.
+        private void fillArray()
+        {
+            Random rnd = new Random();
+
+            for (int lcv = 0; lcv < LEN; ++lcv)
+            {
+                original[lcv] = rnd.Next(1, 100);       //generates the 25 numbers
+                modified[lcv] = original[lcv];          //take those numbers and copy it into its modified array
+            }
+
+            displayArray(original);
+        }
+
+        //This method display the contents of the array that has been passed in (original or modified).
+        //It displays the contents in the ListBox.
+        private void displayArray(int[] a)   //a stands for array
+        {
+            ClearListBox();
+
+            for (int lcv = 0; lcv < LEN; ++lcv)
+            {
+                listBoxArray.Items.Add(a[lcv]);
+            }
+
+        }
+        //this method calls the ListBox's Clear routine
+        //to clear (or empty) out the ListBox
+        private void ClearListBox()
+        {
+            listBoxArray.Items.Clear();
+        }
+        //This is the code that runs when the sum button is clicked
+        private void buttonSum_Click(object sender, EventArgs e)
+        {
+            initializeVariables();
+            calculateTheSumOfArrayElements(1);
+        }
+
+        //This routine initializes the theSum and theAvg 
+        //variables to 0.
+        private void initializeVariables()
+        {
+            theSum = 0;
+            theAvg = 0.0;
+        }
+
+        //This routine will calculate the sum of all array elements and 
+        //if necessary, will also calculate the avg of all array elements.
+        private void calculateTheSumOfArrayElements(int f)
+        {                               //Get sum of all array elements
+            theSum = 0;
+
+            for (int lcv = 0; lcv < LEN; ++lcv)
+            {
+                theSum += original[lcv];
+            }
+            if (f == 1)        //  A "1" herre means the Sum button clicked
+                               //Display sum of all array elements
+            {
+                MessageBox.Show(
+                        "The sum of all array elements:  " + theSum.ToString(),
+                        "SUM OF ALL ARRAY ELEMENTS",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+            }
+            else if (f == 2)       // A "2" here means the Avg button clicked
+            {
+                calculateTheAvgOfArrayElements();
+            }
+        }
+
+        //This routine wil calculate the average of array elements.
+        private void calculateTheAvgOfArrayElements()
+        {
+            theAvg = 0.0;
+            theAvg = theSum / 25.0;
+
+            MessageBox.Show("The average of all array elements: " + theAvg.ToString(),
+                   "AVERAGE OF ALL ARRAY ELEMENTS",
+                   MessageBoxButtons.OK,
+                   MessageBoxIcon.Information);
+        }
+
+        //This is the code that runs when the Clear ListBox button is clicked.
+        private void buttonClearLlistBox_Click(object sender, EventArgs e)
+        {
+            ClearListBox();
+        }
+       
+        //Thisis the code that runs when the Avg button is clicked.
+        private void buttonAvg_Click(object sender, EventArgs e)
+        {
+            calculateTheSumOfArrayElements(2);
+        }
+       
+        //This is the code that runs when the Sort Ascending button is clicked.
+        private void buttonSortAscending_Click(object sender, EventArgs e)
+        {
+            sortAscAndDisplayModified();
+        }
+
+        //This method sorts the array in ascending order, then prints it.
+        private void sortAscAndDisplayModified()
+        {
+            Array.Sort(modified);
+            displayArray(modified);
+        }
+
+        //This is the code that runs when the Lowest button is clicked.
+        private void buttonLowest_Click(object sender, EventArgs e)
+        {
+            findAndDisplayTheLowest();
+        }
+
+        //This routine finds the lowest array element then displays it.
+        private void findAndDisplayTheLowest()
+        {
+            sortAscAndDisplayModified();
+
+            MessageBox.Show(
+                "The smallest array element:  " + modified[0].ToString(),
+                "SMALLEST ARRAY ELEMENT",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        //Thsi is the code that runs when the Sort Descending button is clicked.
+        private void buttonSortDescending_Click(object sender, EventArgs e)
+        {
+            sortDescAndDisplayModified();
+        }
+
+        //This method sorts the array in descending order, then prints it.
+        private void sortDescAndDisplayModified()
+        {
+            Array.Sort(modified);
+            Array.Reverse(modified);
+            displayArray(modified);
+        }
+
+        //This is the code that runs when the Highest button is clicked.
+        private void buttonHighest_Click(object sender, EventArgs e)
+        {
+            findAndDisplayTheHighest();
+        }
+
+        //this routine finds the highest array element then displays it.
+        private void findAndDisplayTheHighest()
+        {
+            sortDescAndDisplayModified();
+
+            MessageBox.Show(
+                "The highest array element:  " + modified[0].ToString(),
+                "HIGHEST ARRAY ELEMENT",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        //This is the code that runs when the Search button is clicked.
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            searchForAnArrayElement();
+        }
+
+        //This button calls a Visual Basic.Net InputBox() function
+        //to let the user search for a value in the array.  It also
+        //displays the result of the search (found or not found).
+        private void searchForAnArrayElement()
+        {
+            sortAscAndDisplayModified();
+
+            string valStr = Interaction.InputBox(
+                            "Number To Search For?",
+                            "SEARCH",
+                            "50", 450, 200);
+
+            int val = Convert.ToInt32(valStr);
+
+            int result = Array.BinarySearch(modified, val);
+
+            if (result < 0)         //Value NOT in array
+            {
+                MessageBox.Show(
+                                    "The value:  " + valStr + " WAS NOT FOUND in the array",
+                                    "VALUE NOT FOUND",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Information);
+            }
+            else
+            {           //value IS in array
+                MessageBox.Show(
+                                "The value:  " + valStr + " WAS FOUND in the array" +
+                                " at position #" + result.ToString(),
+                                "VALUE FOUND",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+            }
+        }
+        //This is the code that runs when the Median button is clicked.
+        private void buttonMedian_Click(object sender, EventArgs e)
+        {
+            findAndDisplayTheMedian();
+        }
+
+        //This code calculates the median then displays it.
+        private void findAndDisplayTheMedian()
+        {
+            sortAscAndDisplayModified();
+
+            MessageBox.Show(
+                               "The  median array element:  " + modified[12].ToString(),
+                               "MEDIAN ARRAY ELEMENT",
+                               MessageBoxButtons.OK,
+                               MessageBoxIcon.Information);
+        }
+
+        //This is the code that runs when the Exit button is clicked.
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            doYouWantToExitNow();
+        }
+
+        //This method allows the user to exit the program.
+        private void doYouWantToExitNow()
+        {
+            if (MessageBox.Show("Exit the progrqam now?",
+                                            "EXIT PROGRAM???",
+                                            MessageBoxButtons.YesNo,
+                                            MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        //This is the code that runs when the Range button is clicked.
+        private void buttonRange_Click_1(object sender, EventArgs e)
+        {
+            calculateAndDisplayTheRange();
+        }
+       
+        private void calculateAndDisplayTheRange()
+        {
+            int theRange = 0;
+
+            sortAscAndDisplayModified();
+
+            theRange = modified[LEN - 1] - modified[0];
+
+            MessageBox.Show(
+                            "The range of the array elements:  " + theRange.ToString(),
+                            "RANGE OF ARRAY ELEMENT",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+        }
+
+        //This is the code that fills the ListBox with values the 
+        //first time the program is run.
+        private void FormArrayFun_Load(object sender, EventArgs e)
+        {
+            fillArray();
+        }          
+    }
+}
